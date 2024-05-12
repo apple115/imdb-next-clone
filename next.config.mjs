0b72ad1e1fcf6ba6832beb214738e4eb79c6891e
apple115 <2567031030@qq.com>
2024-05-13 07:27:55 +0800

    部署在github pages上面

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/imdb-next-clone",
+  output: "export",
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
