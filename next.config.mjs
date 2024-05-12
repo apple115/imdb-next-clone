2d3afc671b89492c15e5c7cd402d4b77781b2890
apple115 <2567031030@qq.com>
2024-05-13 07:45:23 +0800

    fix some bug example: static push

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/imdb-next-clone",
-  output: "export",
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,6 @@ import Results from "@/components/Results";
 const API_KEY = process.env.API_KEY;
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
-
-  // const res = await Promise((resolve) => {
-  //   setTimeout(async () => {
-  //     const response = await fetch(
-  //       `https://api.themoviedb.org/3${
-  //         genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-  //       }?api_key=${API_KEY}&language=en-US&page=1`,
-  //       { next: { revalidate: 10000 } },
-  //     );
-  //     resolve(10000);
-  //   }, 2000);
-  // });
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
